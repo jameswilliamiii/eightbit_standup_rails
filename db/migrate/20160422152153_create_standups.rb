af604fb186e3825f369e4546ccf08c42da7f78b8
James Stubblefield <jameswilliamiii@gmail.com>
2016-04-22 10:47:45 -0500

    Add program_name to Standup

--- db/migrate/20160422152153_create_standups.rb
+++ db/migrate/20160422152153_create_standups.rb
@@ -6,10 +6,12 @@ class CreateStandups < ActiveRecord::Migration
       t.datetime :remind_at, null: false
       t.string   :hipchat_room_name, null: false
       t.integer  :user_id, null: false
+      t.string   :program_name, null: false
 
       t.timestamps null: false
     end
     add_index :standups, :user_id
     add_index :standups, :remind_at
+    add_index :standups, :program_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422151021) do
+ActiveRecord::Schema.define(version: 20160422152925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendee_standups", force: :cascade do |t|
+    t.integer  "attendee_id", null: false
+    t.integer  "standup_id",  null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "attendee_standups", ["attendee_id"], name: "index_attendee_standups_on_attendee_id", using: :btree
+  add_index "attendee_standups", ["standup_id"], name: "index_attendee_standups_on_standup_id", using: :btree
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "hipchat_id"
+    t.string   "hipchat_username"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "attendees", ["hipchat_id"], name: "index_attendees_on_hipchat_id", using: :btree
+
+  create_table "standups", force: :cascade do |t|
+    t.datetime "start_at",          null: false
+    t.datetime "end_at"
+    t.datetime "remind_at",         null: false
+    t.string   "hipchat_room_name", null: false
+    t.integer  "user_id",           null: false
+    t.string   "program_name",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "standups", ["program_name"], name: "index_standups_on_program_name", using: :btree
+  add_index "standups", ["remind_at"], name: "index_standups_on_remind_at", using: :btree
+  add_index "standups", ["user_id"], name: "index_standups_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
